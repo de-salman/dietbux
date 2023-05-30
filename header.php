e725796fcd535e816ed7262d4ebecc5d838efc37
de-salman <salmanfarisi.bca@gmail.com>
2023-05-30 20:00:20 +0530

    font issue

--- header.php
+++ header.php
@@ -8,6 +8,8 @@
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
     <?php wp_head(); ?>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link href="https://fonts.cdnfonts.com/css/gotham" rel="stylesheet">
+
 </head>
 
 <body <?php body_class(); ?>>
